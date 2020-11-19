8f0fa85ce64677f36455e5be7e5db23534f2c2f2
Tega Isiboge <isibogetega@gmail.com>
2020-11-19 22:11:38 +0100

    response classes

---
+++ src/main/java/com/restapi/myblog/payload/response/JwtAuthenticationResponse.java
@@ -0,0 +1,15 @@
+package com.restapi.myblog.payload.response;
+
+import lombok.Data;
+
+@Data
+public class JwtAuthenticationResponse {
+
+    private String accessToken;
+
+    private String tokenType = "Bearer";
+
+    public JwtAuthenticationResponse(String accessToken){
+        this.accessToken = accessToken;
+    }
+}
